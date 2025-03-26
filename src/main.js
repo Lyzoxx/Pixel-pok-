0e86afcab757ee567751ae2df7319222a0e3ea04
Liam <liam11.renaudineau@gmail.com>
2025-03-26 16:57:19 +0100

    update links css and js

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ const button = document.querySelector('#select-button')
 button.addEventListener('click', () => {
     const select = document.querySelector("#Pokemon-select")
     const stylesheet = document.querySelector("#stylesheet")
-    stylesheet.setAttribute("href", `/src/${select.value}.css`)
+    stylesheet.setAttribute("href", `src/${select.value}.css`)
 })
