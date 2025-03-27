b70ec7da82b2b5eeb6eecf7d78f4d6d982a84eec
Liam <liam11.renaudineau@gmail.com>
2025-03-27 11:26:30 +0100

    Pokémon pixel suite, Pokémon en plus

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 const button = document.querySelector('#select-button')
-button.addEventListener('click', () => {
+button.addEventListener('click', selectPokemon)
+
+const reset = document.querySelector('#reset')
+reset.addEventListener('click', resetFunction)
+
+function selectPokemon(){
     const select = document.querySelector("#Pokemon-select")
     const stylesheet = document.querySelector("#stylesheet")
     stylesheet.setAttribute("href", `src/${select.value}.css`)
-})
+}
+
+function resetFunction(){
+    const stylesheet = document.querySelector("#stylesheet")
+    stylesheet.setAttribute("href", "")
+}
